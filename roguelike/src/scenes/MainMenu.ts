3156b48265374f0a87037ba196a1f8b0797baf8d
Rene Schwartz <rene.j.schwartz@gmail.com>
2025-02-16 13:36:21 +0200

    feat: add juice on main menu
    - added couple alpha and position tweens to main menu
    - did some refactoring by adding containers for buttons, this also meant the text is also tweened alongside button sprite

--- roguelike/src/scenes/MainMenu.ts
+++ roguelike/src/scenes/MainMenu.ts
@@ -7,10 +7,15 @@ import { ScreenBackgroundColor } from './ScreenBackgroundColor';
  * Has buttons for starting the game etc.
  */
 export class MainMenu extends Scene {
-    gameTitle: GameObjects.Text;
-    gameName: string = 'Like Rogue RL';
-    /** A simple way to prevent buttons from firing multiple times accidentally. */
-    shouldProcessButtonPresses: boolean = true;
+    /** 
+     * Text gameobject of the title. Subject to bring in alpha tween at the 
+     * beginning of the scene.
+     */
+    private gameTitle: GameObjects.Text;
+    /** Game name placeholder. The game is a roguelike game. */
+    private readonly gameName: string = 'Like Rogue RL';
+    /** A simple way to prevent buttons from firing multiple times. */
+    private shouldProcessButtonPresses: boolean = true;
 
     constructor() {
         super('MainMenu');
@@ -19,39 +24,98 @@ export class MainMenu extends Scene {
     create() {
         const { width, height } = this.scale;
 
-        this.gameTitle = this.add.text(width * 0.5, height * 0.25, this.gameName,
-            {
-                fontFamily: 'Arial Black', fontSize: 42, color: '#ffffff',
-                stroke: '#000000', strokeThickness: 6,
-                align: 'center'
-            })
-            .setOrigin(0.5);
+        // Title
+        this.gameTitle = this.add.text(width * 0.5, height * 0.2,
+            this.gameName, {
+            fontFamily: 'Arial Black', fontSize: 44, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 6, align: 'center'
+        })
+            .setOrigin(0.5)
+            .setAlpha(0);
+        // Title entrance
+        this.add.tween({
+            targets: this.gameTitle,
+            alpha: 1,
+            ease: 'cubic.in',
+            duration: 2000,
+            delay: 500
+        });
 
-        // Add Main Menu's main buttons: 
-        // New Game, High Scores, Credits, Quit Game.
-        // Buttons are 'three sliced' for perf. The height can't be modified.
-        const newGameButton = this.add.nineslice(width * 0.5, height * 0.5, 'ui-border-1', 0, 96, 0, 20, 20)
+        // Main Menu's main buttons: 
+        // New Game, High Scores, Credits, Quit Game etc.
+        // Buttons are 'three sliced' for perf and simplicity. 
+        // Height can't be modified on three sliced images.
+        // Further vertical layoyt group could be made with 
+        // Actions.GridAlign(items, options) with width 1 grid
+        // or maybe AlignTo(items, position, [offsetX], [offsetY]) if it works.
+
+        // New Game Button
+        const newGameButtonContainer = this.add.container(width * 1.25, height * 0.5);
+        const newGameButtonImage = this.add.nineslice(0, 0,
+            'ui-border-1', 0, 96, 0, 20, 20)
             .setSize(width * 0.3, 0)
             .setInteractive()
-            .on('pointerdown', () => this.onLoadNewGameButtonClicked())
-            .on('pointerover', () => newGameButton.scale = 1.07)
-            .on('pointerout', () => newGameButton.scale = 1);
-        this.add.text(newGameButton.x, newGameButton.y, 'New Game')
+            .on('pointerdown', () => {
+                this.onLoadNewGameButtonClicked();
+                this.buttonsExitTween();
+            })
+            .on('pointerover', () => newGameButtonContainer.scale = 1.07)
+            .on('pointerout', () => newGameButtonContainer.scale = 1);
+        const newGameButtonText = this.add.text(0, 0, 'New Game')
             .setOrigin(0.5)
             .setStyle({ fontSize: 38 });
+        newGameButtonContainer.add([newGameButtonImage, newGameButtonText])
+            .setAlpha(0);
 
-        const quitGameButton = this.add.nineslice(width * 0.5, height * 0.65, 'ui-border-1', 0, 96, 0, 20, 20)
+        // Quit Game Button
+        const quitGameButtonContainer = this.add.container(width * 1.5, height * 0.65);
+        const quitGameButtonImage = this.add.nineslice(0, 0,
+            'ui-border-1', 0, 96, 0, 20, 20)
             .setSize(width * 0.3, 0)
             .setInteractive()
             .on('pointerdown', () => console.log("Quit Game"))
-            .on('pointerover', () => quitGameButton.scale = 1.07)
-            .on('pointerout', () => quitGameButton.scale = 1);
-        this.add.text(quitGameButton.x, quitGameButton.y, 'Quit Game')
+            .on('pointerover', () => quitGameButtonContainer.scale = 1.07)
+            .on('pointerout', () => quitGameButtonContainer.scale = 1);
+        const quitGameButtonText = this.add.text(0, 0, 'Quit Game')
             .setOrigin(0.5)
             .setStyle({ fontSize: 38 });
+        quitGameButtonContainer.add([quitGameButtonImage, quitGameButtonText])
+            .setAlpha(0);
+
+        this.buttonsEntranceTweens([quitGameButtonContainer, newGameButtonContainer], width * 0.87);
     }
 
-    /** Loads a new game when button is pressed. Also initiates some visual effects. */
+    /** Buttons entrance from the left side. */
+    private buttonsEntranceTweens(targets: GameObjects.Container[], position: number): void {
+        this.add.tween({
+            targets: targets,
+            x: position,
+            ease: 'cubic.out',
+            duration: 2000,
+            onComplete: () => {  },
+            delay: 500,
+        });
+        this.add.tween({
+            targets: targets,
+            alpha: { from: 0, to: 1 },
+            ease: 'cubic.in',
+            duration: 2000,
+            delay: 500
+        });
+    }
+
+    /** When a new game is loaded the buttons slide away during transition. */
+    private buttonsExitTween(): void {
+        this.children.list.filter(x => x instanceof Phaser.GameObjects.Container)
+            .forEach(element => this.add.tween({
+                targets: element,
+                x: element.x + 200,
+                ease: 'cubic.out',
+                duration: 500,
+            }))
+    }
+
+    /** Loads a new game and initiates visual effects. */
     private onLoadNewGameButtonClicked(): void {
         console.log(this.onLoadNewGameButtonClicked.name);
 
