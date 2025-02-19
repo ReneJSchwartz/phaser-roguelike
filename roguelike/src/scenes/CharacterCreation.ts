9316c388e18f959cf6926e5bc79b0ad55681d02e
Rene Schwartz <rene.j.schwartz@gmail.com>
2025-02-19 03:20:55 +0200

    chore: add rexUI plugin
    - rexUI is "The" UI plugin for Phaser. documentation and examples are lacking so well see if we're going to use this or not.

--- roguelike/src/scenes/CharacterCreation.ts
+++ roguelike/src/scenes/CharacterCreation.ts
@@ -1,6 +1,7 @@
 import { Scene, GameObjects } from 'phaser';
 import { Game } from './Game';
 import { ScreenBackgroundColor } from './ScreenBackgroundColor';
+import RexUIPlugin from 'phaser3-rex-plugins/templates/ui/ui-plugin.js';
 
 /** 
  * Character creation view.
@@ -20,12 +21,17 @@ export class CharacterCreation extends Scene {
     /** A simple way to prevent buttons from firing multiple times. */
     private shouldProcessButtonPresses: boolean = true;
 
+    private rexUI: RexUIPlugin;
+
     constructor() {
         super('CharacterCreation');
     }
 
     create() {
         const { width, height } = this.scale;
+        /** Font y-size. */
+        const em: number = 28
+        const padding: number = em * 0.5;
 
         this.screenTitle = this.add.text(width * 0.5, height * 0.05,
             this.screenName, {
@@ -38,24 +44,44 @@ export class CharacterCreation extends Scene {
         // Different selection sections in the screen are in their containers
         // for movement and filtering
         const ancestrySectionContainer = this.add.container(width * 0.05, height * 0.15);
-        const ancestryBg = this.add.image(0,0, '')
-            .setScale(10, 10)
-            .setOrigin(0, 0)
-            .setAlpha(0.5);
-        const ancestrySectionTitle = this.add.text(0, 0, 'Ancestry')
+        const ancestryBoxOutline = this.add.rectangle(0, 0, width * 0.15, height * 0.25)
+            .setOrigin(0)
+            .setStrokeStyle(1, 0xffffff);
+        const ancestrySectionTitle = this.add.text(padding, padding, 'Ancestry')
             .setOrigin(0, 0)
             .setStyle({ fontSize: 32 });
-        const ancestryOptions = this.add.text(0, 40, '> Human\n  Catfolk')
-            .setOrigin(0, 0)
+        const ancestryOptions = this.add.text(padding, em * 2, '> Human\n  Catfolk')
+            .setOrigin(0, 0) 
             .setStyle({ fontSize: 28 });
-        ancestrySectionContainer.add([ancestryBg, ancestrySectionTitle, ancestryOptions])
+        ancestrySectionContainer.add([ancestryBoxOutline, ancestrySectionTitle, ancestryOptions])
             .setAlpha(0);
 
+        const infoBoxSectionContainer = this.add.container(width * 0.70, height * 0.1);
+        const infoBoxBg = this.add.rectangle(0, 0, width * 0.25, height * 0.8)
+            .setOrigin(0)
+            .setStrokeStyle(1, 0xffffff);
+        const infoBoxTitle = this.add.text(0, 0, 'Info - Catfolk')
+            .setOrigin(0, 0)
+            .setStyle({ fontSize: 32 });
+
+        const hmm = this.add.text(0, em * 2.5, 'Lorem Ipsum Dolor Sit Amet.\n\nSecond Paragraph is veery long and it just continues.')
+            .setOrigin(0, 0)
+            .setStyle({ fontSize: 28 });
+        const infoBoxContent = this.rexUI.add.textArea({ width: 50, height: 300, text: hmm });
+        infoBoxSectionContainer.add([infoBoxBg, infoBoxTitle, infoBoxContent]);
+
+
+        // this.scene.
 
         this.tweens.add({
             targets: this.children.list,
             alpha: 1,
         });
+    }
+
+    /** Shows text at the info box. */
+    private setTextToInfoBox(text: string) {
+
     }
 
     /** Loads a new game and initiates visual effects. */
