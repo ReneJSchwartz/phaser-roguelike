a53a4dc37bbf087d031d3db100f3348b551ca17a
Rene Schwartz <rene.j.schwartz@gmail.com>
2025-02-19 23:21:05 +0200

    feat: add ancestries
    - there's now also a vertical list of the selectable races that when selected show the race info
    - selecting random ancestry does just that

--- roguelike/src/character-creation/ancestries.ts
+++ roguelike/src/character-creation/ancestries.ts
@@ -1,6 +1,6 @@
 /** 
- * Ancestry is a data model about a particular ancestry such as Half-Orc.
- * Is used in char creation and possibly later in-game. 
+ * Ancestry holds information about a particular ancestry such as Half-Orc.
+ * Is used in char creation and later in-game. 
  */
 export class Ancestry {
     public name: string;
@@ -22,18 +22,33 @@ export class Ancestry {
 /** Contains data about available ancestries for character creation. */
 export class Ancestries {
     public static human: Ancestry = new Ancestry('Human',
-        `Humans are adaptive folk well spread across continents that usually undergo an extensive education.
+        `Humans are adaptive folk well spread across continents that usually undergo an extensive education. They are fast learners.
 
 Humans in general don’t have any apparent strengths or weaknesses but individuals can excel in various ways. They prosper because of their differences. What sets you apart?
 
-At least one Attribute point in one of these stats is required to play as a human: Kno, Dex, Spi`);
+At least one Attribute point in Kno or Dex or Spi is required to play as a human.`);
     /** Catfolk also have Perks Nine Lives and Darkvision, Keen Senses etc. */
-    public static catfolk: Ancestry = new Ancestry('Catfolk', 
-        `Catfolk are nimble bipedal combatants that speak funny but scratch mightily. Some are more cat-like and others resemble tigers or panthers.
+    public static catfolk: Ancestry = new Ancestry('Catfolk',
+        `Catfolk are nimble bipedal predators that speak funnily but scratch mightily. Some are more cat-like and others resemble tigers or panthers.
         
-They seem to avoid death as well as they avoid hits. They have excellent senses. In their silent deadly hunt they have been said to have mastered the Predator technique.
+Catfolk avoid death as well as they avoid hits and they have excellent senses. Gain access to Perks 'Nine Lives' and 'Keen Senses'. These great cats make good mages as well due to their connection with the nature.
     
-To play as one put at least one attribute point to Dex and another to Dex or Str.`)
-    // ['Dwarf', 'Catfolk', 'House-elf']);
+To play as one put at least one attribute point to Dex and another to Dex or Str.`);
+    public static houseElf: Ancestry = new Ancestry('House-elf',
+        `House-elves have many names. They are small, helpful but stubborn and very magical folk. Their natural abilities include turning invisible or into an animal. You gain those Perks.
 
+For some reason they don't like clothes. They rather go in rags or even naked if they are hairy enough. 
+
+To play as one put one point into Spi and another point into Spi/Dex/Kno.`);
+    public static dwarf: Ancestry = new Ancestry('Dwarf', `Dwarves are hardy mountain or hill-dwellers, skilled craftsmen and knowledgeable.
+
+Dwarves have two poison resistance perks as well as darkvision and 
+
+To play as one put one point into Con and another point into Con/Str/Spi/Kno.`);
+
+    public static gnome: Ancestry = new Ancestry('Gnome', `Gnomes are small and intelligent. They can be for example tinkerers, researchers or jewelers.
+    
+To play as one put one point into Kno and another point into Kno/Con/Dex.`);
+
+    public static random: Ancestry = new Ancestry('Random', '');
 }
--- roguelike/src/scenes/CharacterCreation.ts
+++ roguelike/src/scenes/CharacterCreation.ts
@@ -1,7 +1,8 @@
-import { Scene, GameObjects } from 'phaser';
+import { Scene, GameObjects, Actions } from 'phaser';
 import { Game } from './Game';
 import { ScreenBackgroundColor } from './ScreenBackgroundColor';
-import { Ancestries } from '../character-creation/ancestries';
+import { Ancestries, Ancestry } from '../character-creation/ancestries';
+import RexUIPlugin from 'phaser3-rex-plugins/templates/ui/ui-plugin.js'
 
 /** 
  * Character creation view.
@@ -11,10 +12,16 @@ import { Ancestries } from '../character-creation/ancestries';
  * The player also selects or randomizes their name.
  */
 export class CharacterCreation extends Scene {
-    /** View title is at top middle. */
+    /** View title is at top middle of the screen. */
     private readonly screenName: string = 'Character Creation';
     /** A simple way to prevent buttons from firing multiple times. */
     private shouldProcessButtonPresses: boolean = true;
+    /** Used to represent info about something in the screen. */
+    private infoBoxContent: GameObjects.Text;
+    /** Infobox title needs to be synced with @see infoBoxContent. */
+    private infoBoxTitle: GameObjects.Text;
+
+    private rexUI: RexUIPlugin;
 
     constructor() {
         super('CharacterCreation');
@@ -24,6 +31,7 @@ export class CharacterCreation extends Scene {
         const { width, height } = this.scale;
         /** Font y-size. */
         const em: number = 28
+        /** Padding for text from section borders. */
         const padding: number = em * 0.5;
 
         // View title
@@ -36,19 +44,28 @@ export class CharacterCreation extends Scene {
             .setAlpha(0);
 
         // Different containerized selection sections in the screen
-        
+
         // Ancestry is the first thing on top left
-        const ancestrySectionContainer = this.add.container(width * 0.04, height * 0.15);
-        const ancestryBoxOutline = this.add.rectangle(0, 0, width * 0.15, height * 0.25)
+        const ancestrySectionContainer = this.add.container(width * 0.04, height * 0.10);
+        const ancestryBoxOutline = this.add.rectangle(0, 0, width * 0.15, height * 0.26)
             .setOrigin(0)
             .setStrokeStyle(1, 0xffffff);
         const ancestrySectionTitle = this.add.text(padding, padding, 'Ancestry')
             .setOrigin(0, 0)
             .setStyle({ fontSize: 32 });
-        const ancestryOptions = this.add.text(padding, em * 2, '> Human\n  Catfolk')
-            .setOrigin(0, 0)
-            .setStyle({ fontSize: 28 });
-        ancestrySectionContainer.add([ancestryBoxOutline, ancestrySectionTitle, ancestryOptions])
+        // Make ancestries list.
+        const selectableAncestries: Ancestry[] = [Ancestries.human, Ancestries.catfolk, Ancestries.houseElf, Ancestries.dwarf, Ancestries.gnome, Ancestries.random];
+        const ancestriesTexts: GameObjects.Text[] = [];
+        for (let index = 0; index < selectableAncestries.length; index++) {
+            const t = this.add.text(padding, em * 2 + index * em, '  ' + selectableAncestries[index].name)
+                .setOrigin(0, 0)
+                .setStyle({ fontSize: 28 })
+                .setInteractive()
+                .on('pointerdown', () => this.selectAncestry(selectableAncestries[index], ancestriesTexts, index));
+            ancestriesTexts.push(t);
+            ancestrySectionContainer.add(t);
+        }
+        ancestrySectionContainer.add([ancestryBoxOutline, ancestrySectionTitle])
             .setAlpha(0);
 
         // Info box provides info on what is currently selected.
@@ -57,19 +74,42 @@ export class CharacterCreation extends Scene {
         const infoBoxBg = this.add.rectangle(0, 0, width * 0.26, height * 0.8)
             .setOrigin(0)
             .setStrokeStyle(1, 0xffffff);
-        const infoBoxTitle = this.add.text(padding, padding, 'Info - Human')
+        this.infoBoxTitle = this.add.text(padding, padding, 'Info - Human')
             .setOrigin(0, 0)
             .setStyle({ fontSize: 32 });
-        const infoBoxContent = this.add.text(padding, em * 2.5, Ancestries.catfolk.description)
+        this.infoBoxContent = this.add.text(padding, em * 2.5, Ancestries.catfolk.description)
             .setOrigin(0, 0)
             .setStyle({ fontSize: 28, wordWrap: { width: width * 0.245 } });
-        infoBoxSectionContainer.add([infoBoxBg, infoBoxTitle, infoBoxContent]);
+        infoBoxSectionContainer.add([infoBoxBg, this.infoBoxTitle, this.infoBoxContent]);
 
         this.tweens.add({
             targets: this.children.list,
             alpha: 1
         });
+
+        this.selectAncestry(selectableAncestries[0], ancestriesTexts, 0);
     }
+
+    /** Populates info box and possibly controls attribute selection requirements. */
+    private selectAncestry(ancestry: Ancestry, options: GameObjects.Text[], index: number): void {
+        console.log(ancestry.name);
+        if (ancestry.name === 'Random') {
+            const selectableAncestries: Ancestry[] = [Ancestries.human, Ancestries.catfolk, Ancestries.houseElf, Ancestries.dwarf, Ancestries.gnome, Ancestries.random];
+            const randInt: number = Phaser.Math.Between(0, selectableAncestries.length - 2); 
+            this.selectAncestry(selectableAncestries[randInt], options, randInt);
+            return;
+        }
+
+
+        this.infoBoxTitle.text = 'Info - ' + ancestry.name;
+        this.infoBoxContent.text = ancestry.description;
+
+        // Visually show that the new ancestry is selected and the old ones are not.
+        for (let i = 0; i < options.length; i++) {
+            options[i].setColor(i === index ? '#fff' : '#666');
+        }
+    }
+
 
     /** Loads a new game and initiates visual effects. */
     private onStartNewGameButtonClicked(): void {
