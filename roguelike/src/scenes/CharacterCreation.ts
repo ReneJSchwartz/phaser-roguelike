fb75e095972a0f84608032cd9c29f8c660d287df
Rene Schwartz <rene.j.schwartz@gmail.com>
2025-02-20 14:59:47 +0200

    feat: char creation buttons & sections
    - added button for going back to menu or the the game and fixed state bugs with them so the views can be triggered multiple times
    - start to game button goes to game
    - randomize everything button started
    - added sections for stats and a coming soon secction for the chosen layout (see code comments)

--- roguelike/src/scenes/CharacterCreation.ts
+++ roguelike/src/scenes/CharacterCreation.ts
@@ -3,6 +3,7 @@ import { Game } from './Game';
 import { ScreenBackgroundColor } from './ScreenBackgroundColor';
 import { Ancestries, Ancestry } from '../character-creation/ancestries';
 import RexUIPlugin from 'phaser3-rex-plugins/templates/ui/ui-plugin.js'
+import { MainMenu } from './MainMenu';
 
 /** 
  * Character creation view.
@@ -20,7 +21,9 @@ export class CharacterCreation extends Scene {
     private infoBoxContent: GameObjects.Text;
     /** Infobox title needs to be synced with @see infoBoxContent. */
     private infoBoxTitle: GameObjects.Text;
-
+    /** Saved so that randomization can pick a different new random ancestry every time. */
+    private lastChosenAncestryIndex: number = 42;
+    /** Has UI components for example for layout groups or a text field. */
     private rexUI: RexUIPlugin;
 
     constructor() {
@@ -28,6 +31,8 @@ export class CharacterCreation extends Scene {
     }
 
     create() {
+        this.shouldProcessButtonPresses = true;
+
         const { width, height } = this.scale;
         /** Font y-size. */
         const em: number = 28
@@ -46,11 +51,11 @@ export class CharacterCreation extends Scene {
         // Different containerized selection sections in the screen
 
         // Ancestry is the first thing on top left
-        const ancestrySectionContainer = this.add.container(width * 0.04, height * 0.10);
-        const ancestryBoxOutline = this.add.rectangle(0, 0, width * 0.15, height * 0.26)
+        const ancestrySectionContainer: GameObjects.Container = this.add.container(width * 0.04, height * 0.10);
+        const ancestryBoxOutline: GameObjects.Rectangle = this.add.rectangle(0, 0, width * 0.15, height * 0.26)
             .setOrigin(0)
             .setStrokeStyle(1, 0xffffff);
-        const ancestrySectionTitle = this.add.text(padding, padding, 'Ancestry')
+        const ancestrySectionTitle: GameObjects.Text = this.add.text(padding, padding, 'Ancestry')
             .setOrigin(0, 0)
             .setStyle({ fontSize: 32 });
         // Make ancestries list.
@@ -68,10 +73,37 @@ export class CharacterCreation extends Scene {
         ancestrySectionContainer.add([ancestryBoxOutline, ancestrySectionTitle])
             .setAlpha(0);
 
-        // Info box provides info on what is currently selected.
-        const infoBoxSectionContainer = this.add.container(width * 0.70, height * 0.1)
+        // Next section can be for backgrounds or inventory or spells or starting equipment
+        const nextSectionContainer: GameObjects.Container = this.add.container(width * 0.21, height * 0.10);
+        const nextSectionOutline: GameObjects.Rectangle = this.add.rectangle(0, 0, width * 0.42, height * 0.26)
+            .setOrigin(0)
+            .setStrokeStyle(1, 0xffffff);
+        const nextSectionTitle: GameObjects.Text = this.add.text(padding, padding, 'Coming soon!')
+            .setOrigin(0, 0)
+            .setStyle({ fontSize: 32 });
+        nextSectionContainer.add([nextSectionOutline, nextSectionTitle])
             .setAlpha(0);
-        const infoBoxBg = this.add.rectangle(0, 0, width * 0.26, height * 0.8)
+
+        // Attribute points section.
+        const attributesSectionContainer: GameObjects.Container = this.add.container(width * 0.04, height * 0.395);
+        const attributesSectionOutline: GameObjects.Rectangle = this.add.rectangle(0, 0, width * 0.59, height * 0.35)
+            .setOrigin(0)
+            .setStrokeStyle(1, 0xffffff);
+        const attributesSectionTitle = this.add.text(padding, padding, 'Attributes (5 remaining)')
+            .setOrigin(0, 0)
+            .setStyle({ fontSize: 32 });
+        const attributesSectionRandomizeText = this.add.text(width * 0.35, padding, '[Randomize]')
+            .setOrigin(0)
+            .setStyle({ fontSize: 28 })
+            .setInteractive()
+            .on('pointerdown', () => { console.log('test');/* randomizeAttributePoints(); */ });
+        attributesSectionContainer.add([attributesSectionOutline, attributesSectionTitle, attributesSectionRandomizeText])
+            .setAlpha(0);
+
+        // Info box provides info on what is currently selected.
+        const infoBoxSectionContainer: GameObjects.Container = this.add.container(width * 0.65, height * 0.1)
+            .setAlpha(0);
+        const infoBoxOutline: GameObjects.Rectangle = this.add.rectangle(0, 0, width * 0.31, height * 0.8)
             .setOrigin(0)
             .setStrokeStyle(1, 0xffffff);
         this.infoBoxTitle = this.add.text(padding, padding, 'Info - Human')
@@ -79,8 +111,27 @@ export class CharacterCreation extends Scene {
             .setStyle({ fontSize: 32 });
         this.infoBoxContent = this.add.text(padding, em * 2.5, Ancestries.catfolk.description)
             .setOrigin(0, 0)
-            .setStyle({ fontSize: 28, wordWrap: { width: width * 0.245 } });
-        infoBoxSectionContainer.add([infoBoxBg, this.infoBoxTitle, this.infoBoxContent]);
+            .setStyle({ fontSize: 28, wordWrap: { width: width * 0.3 } });
+        infoBoxSectionContainer.add([infoBoxOutline, this.infoBoxTitle, this.infoBoxContent]);
+
+        // Bottom text buttons for starting the game, randomizing everything or going back to menu.
+        const startGameButton: GameObjects.Text = this.add.text(width * 0.04, height * 0.93, 'Start Game')
+            .setOrigin(0)
+            .setStyle({ fontSize: 32 })
+            .setInteractive()
+            .on('pointerdown', () => { this.onFinishCharacterCreationAndStartNewGameButtonClicked(); });
+
+        const randomizeEverythingButton: GameObjects.Text = this.add.text(width * 0.22, height * 0.93, 'Randomize Everything')
+            .setOrigin(0)
+            .setStyle({ fontSize: 32 })
+            .setInteractive()
+            .on('pointerdown', () => { this.onRandomizeEverythingButtonClicked(); });
+
+        const backToMenuButton: GameObjects.Text = this.add.text(width * 0.52, height * 0.93, 'Back To Menu')
+            .setOrigin(0)
+            .setStyle({ fontSize: 32 })
+            .setInteractive()
+            .on('pointerdown', () => { this.onBackToMenuButtonClicked(); });
 
         this.tweens.add({
             targets: this.children.list,
@@ -90,17 +141,27 @@ export class CharacterCreation extends Scene {
         this.selectAncestry(selectableAncestries[0], ancestriesTexts, 0);
     }
 
-    /** Populates info box and possibly controls attribute selection requirements. */
+    /** 
+     * Populates info box and possibly controls attribute selection requirements. '
+     * 
+     * @param ancestry What ancestry was chosen for the character.
+     * @param options The different ancestry gameobject texts for styling purposes.
+     * @param index what was the index of the chosen option. Used for styling.
+     */
     private selectAncestry(ancestry: Ancestry, options: GameObjects.Text[], index: number): void {
-        console.log(ancestry.name);
         if (ancestry.name === 'Random') {
             const selectableAncestries: Ancestry[] = [Ancestries.human, Ancestries.catfolk, Ancestries.houseElf, Ancestries.dwarf, Ancestries.gnome, Ancestries.random];
-            const randInt: number = Phaser.Math.Between(0, selectableAncestries.length - 2); 
+
+            let randInt: number;// = Phaser.Math.Between(0, selectableAncestries.length - 2);
+            do {
+                randInt = Phaser.Math.Between(0, selectableAncestries.length - 2);
+            } while (randInt === this.lastChosenAncestryIndex)
+
             this.selectAncestry(selectableAncestries[randInt], options, randInt);
             return;
         }
 
-
+        this.lastChosenAncestryIndex = index;
         this.infoBoxTitle.text = 'Info - ' + ancestry.name;
         this.infoBoxContent.text = ancestry.description;
 
@@ -110,10 +171,41 @@ export class CharacterCreation extends Scene {
         }
     }
 
+    /** Randomizes ancestry, attribute points and other things in the screen if any such as name. */
+    private onRandomizeEverythingButtonClicked(): void {
+        console.log(this.onRandomizeEverythingButtonClicked.name);
 
-    /** Loads a new game and initiates visual effects. */
-    private onStartNewGameButtonClicked(): void {
-        console.log(this.onStartNewGameButtonClicked.name);
+        // todo reroll ancestry
+
+        // todo randomize attributes
+    }
+
+    private onBackToMenuButtonClicked(): void {
+        console.log(this.onBackToMenuButtonClicked.name);
+
+        // Guard clause against spamming buttons.
+        if (this.shouldProcessButtonPresses === false) {
+            return;
+        }
+        this.shouldProcessButtonPresses = false;
+
+        // Visual effects for scene transition.
+        ScreenBackgroundColor.instance.briefWhiteFlash();
+
+        this.children.list.forEach(element => {
+            this.tweens.add({
+                targets: element,
+                alpha: 0,
+                duration: 250
+            })
+        });
+
+        this.time.delayedCall(550, () => this.scene.start(MainMenu.name));
+    }
+
+    /** Starts a new game with the selected character settings and initiates visual effects. */
+    private onFinishCharacterCreationAndStartNewGameButtonClicked(): void {
+        console.log(this.onFinishCharacterCreationAndStartNewGameButtonClicked.name);
 
         // Guard clause against spamming buttons.
         if (this.shouldProcessButtonPresses === false) {
